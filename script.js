9ef84661200fa44d633c1d0dcfa32d0408d3dd1e
Роберт Шевцов <55320224+RobertUA@users.noreply.github.com>
2020-10-09 15:54:31 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function OnClick()
-{
-    document.getElementById("div").textContent = three(document.getElementById ("inp").value);
-}
-function three(str)//прост захотілось
-{
-    return str[0].toUpperCase()+str.slice(1);
-}
